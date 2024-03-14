888386951899dc4e1606f9025108b719a716d9ff
Maksim Ustinov <ustiinovmp@gmail.com>
2024-03-14 07:50:49 +0300

    edited tests

--- scrapper/src/test/java/edu/java/scrapper/DatabaseMigrationTest.java
+++ scrapper/src/test/java/edu/java/scrapper/DatabaseMigrationTest.java
@@ -2,6 +2,7 @@ package edu.java.scrapper;
 
 import java.sql.Connection;
 import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import lombok.SneakyThrows;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
@@ -11,19 +12,27 @@ public class DatabaseMigrationTest extends IntegrationEnvironment {
     @Test
     @DisplayName("Chat table test")
     @SneakyThrows
-    public void migrationChat_shouldWorkCorrectly() {
+    public void migrationChatTable_shouldWorkCorrectly() {
+        //Arrange
         Connection connection = POSTGRES.createConnection("");
         PreparedStatement statement = connection.prepareStatement("SELECT * FROM chat");
-        assertThat(statement.executeQuery().getMetaData().getColumnName(1)).isEqualTo("chat_id");
+        //Act
+        ResultSet resultSet = statement.executeQuery();
+        //Assert
+        String columnName = resultSet.getMetaData().getColumnName(1);
+        assertThat(columnName).isEqualTo("chat_id");
     }
 
     @Test
     @DisplayName("Link table test")
     @SneakyThrows
-    public void migrationLink_shouldWorkCorrectly() {
+    public void migrationLinkTable_shouldWorkCorrectly() {
+        //Arrange
         Connection connection = POSTGRES.createConnection("");
         PreparedStatement statement = connection.prepareStatement("SELECT * FROM link");
+        //Act
         var meta = statement.executeQuery().getMetaData();
+        //Assert
         assertThat(meta.getColumnName(1)).isEqualTo("link_id");
         assertThat(meta.getColumnName(2)).isEqualTo("last_update_time");
         assertThat(meta.getColumnName(3)).isEqualTo("name");

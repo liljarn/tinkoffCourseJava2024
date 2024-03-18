34ea4de09bac1feecb4c273c20eaf1f9bd32f86f
Maksim Ustinov <ustiinovmp@gmail.com>
2024-03-18 03:15:13 +0300

    fixing issues

--- scrapper/src/main/java/edu/java/client/github/GitHubInfoProvider.java
+++ scrapper/src/main/java/edu/java/client/github/GitHubInfoProvider.java
@@ -13,7 +13,7 @@ import org.springframework.beans.factory.annotation.Value;
 @Log4j2
 public class GitHubInfoProvider extends WebClientInfoProvider {
     private static final String BASE_API_URL = "https://api.github.com/repos";
-    private static final String EVENTS_ENDPOINT = "/events??per_page=10";
+    private static final String EVENTS_ENDPOINT = "/events?per_page=10";
     private static final Pattern GITHUB_PATTERN = Pattern.compile("https://github.com/(.+)/(.+)");
 
     @Value("${client.github.token}")

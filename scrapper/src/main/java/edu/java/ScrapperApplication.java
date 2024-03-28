5f71b4158a18c1189bad8c25fcb56933a576934c
Maksim Ustinov <ustiinovmp@gmail.com>
2024-03-28 06:13:30 +0300

    added bucket4j rate limit

--- scrapper/src/main/java/edu/java/ScrapperApplication.java
+++ scrapper/src/main/java/edu/java/ScrapperApplication.java
@@ -4,11 +4,13 @@ import edu.java.configuration.ApplicationConfig;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.scheduling.annotation.EnableScheduling;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 @SpringBootApplication
 @EnableScheduling
+@EnableCaching
 @EnableConfigurationProperties(ApplicationConfig.class)
 @EnableTransactionManagement
 public class ScrapperApplication {
